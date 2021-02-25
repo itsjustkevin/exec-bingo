306bb4e3522972b7e9857641da9474e101a1cf1c
itsjustkevin <kevinjchisholm@gmail.com>
2021-02-24 20:11:15 -0600

    cells selectable

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="grid grid-cols-5 gap-4 w-40 h-40">
+      <div v-for="(word, index) in wordList" :key="index">
+        <cell :word="word"></cell>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Cell from './components/Cell.vue'
+
+export default {
+  components: {
+    Cell
+  },
+  data() {
+    return {
+      wordList: [],
+    };
+  },
+
+  methods: {
+    getWordList() {
+      for (let i = 1; i < 26; i++) {
+        this.wordList.push(i);
+      }
+    },
+  },
+
+  created() {
+    this.getWordList();
+  },
+};
+</script>
---
+++ src/components/Cell.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="mark(index)" :class="[selected ? 'bg-gray-400' : '']">
+    {{ word }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "cell",
+
+  props: ["selected", "word"],
+
+  data() {
+    return {
+      selected: false,
+    };
+  },
+
+  methods: {
+    mark(cell) {
+      this.selected = !this.selected;
+    },
+  }
+};
+</script>
