2569a6dd2dc866fdeb5d3715383112de397ac457
itsjustkevin <kevinjchisholm@gmail.com>
2021-02-25 22:38:57 -0600

    Update text color

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,8 +1,8 @@
 <template>
   <div
     class="bg-white sm:h-square sm:w-square flex justify-center items-center"
-    @click="mark(index)"
-    :class="[selected ? 'bg-google-blue text-white' : '']"
+    @click="mark()"
+    :class="[selected ? 'bg-google-blue text-white' : 'text-gray-500']"
   >
     {{ word }}
   </div>
@@ -12,7 +12,9 @@
 export default {
   name: "cell",
 
-  props: ["word"],
+  props: {
+    word: String
+  },
 
   data() {
     return {
