8625efab8e39ffed63b6a03bb9f09953d13b0bd0
itsjustkevin <kevinjchisholm@gmail.com>
2021-02-27 19:13:37 -0600

    add toggle button component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div class="text-center text-2xl font-mono font-bold py-4 dark:text-gray-200 text-google-blue">
       Executive Lingo Bingo
     </div>
+    <toggle></toggle>
     <div class="dark:bg-gray-900 bg-gray-200 m-4 p-4 sm:h-board sm:w-board">
       <div class="grid grid-cols-5 gap-4 text-center">
         <div v-for="word in wordList" :key="word.id">
@@ -15,12 +16,14 @@
 
 <script>
 import Cell from "./components/Cell.vue";
+import Toggle from "./components/Toggle.vue";
 import _ from "lodash";
 import wordBank from "../data/wordlist.js";
 
 export default {
   components: {
     Cell,
+    Toggle,
   },
 
   data() {
---
+++ src/components/Toggle.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="flex bg-gray-300 h-8 w-16 rounded-full"
+        :class="[selected ? 'justify-end transition duration-200 ease-in-out' : '']"
+    >
+        <button class="relative align-end h-8 w-8 rounded-full bg-google-red"
+            @click="toggle"
+        ></button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            selected: false
+        }
+    },
+
+    methods: {
+        toggle() {
+            this.selected = ! this.selected;
+        }
+    }
+}
+</script>
